e5127282e020167fb149297a128c98e34813e468
Milla Kelhu <milla.kelhu@gmail.com>
2021-11-29 15:15:23 +0200

    Get rid of circular import

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
-from flask import Flask
+from routes import app
 
-app = Flask(__name__)
-
-import routes
+if __name__ == "__main__":
+    app.run()
--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
-from app import app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 
 @app.route("/")
 def index():
