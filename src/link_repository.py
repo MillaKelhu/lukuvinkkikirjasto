efec173a7c9622656b0da032391c5557ce180dc2
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2021-11-29 15:15:08 +0200

    fix linter complains for good

--- src/link_repository.py
+++ src/link_repository.py
@@ -1,53 +1,62 @@
 class LinkRepository:
-    """Konstruktoi sqlalchemy-sessiosta linkki-repositorion"""
     def __init__(self, session):
+        """Konstruktoi sqlalchemy-sessiosta linkki-repositorion"""
+
         self.session = session
 
-    """Hakee linkin tietokannasta Pythonin dictionary-olion id-kentän
-    perusteella ja palauttaa kyseisen rivin
-    sqlalchemy.engine.RowProxy-oliona"""
     def find(self, link):
+        """Hakee linkin tietokannasta Pythonin dictionary-olion id-kentän
+        perusteella ja palauttaa kyseisen rivin
+        sqlalchemy.engine.RowProxy-oliona"""
         query = "SELECT * FROM Links WHERE id = :id"
+
         return self.session.execute(query, {"id": link["id"]}).fetchone()
 
-    """Hakeee kaikki linkit tietokannasta ja palauttaa ne listana
-    sqlalchemy.engine.RowProxy-olioita"""
     def find_all(self):
+        """Hakeee kaikki linkit tietokannasta ja palauttaa ne listana
+        sqlalchemy.engine.RowProxy-olioita"""
+
         return self.session.execute("SELECT * FROM Links").fetchall()
 
-    """Luo linkki tietokantaan Pythonin dictionary-olion title- ja
-    link_url-kentistä.
-    Huom. Metodin kutsujan vastuulla on kutsua commit-metodia
-    muutosten tallentamiseksi"""
     def create(self, link):
+        """Luo linkki tietokantaan Pythonin dictionary-olion title- ja
+        link_url-kentistä.
+        Huom. Metodin kutsujan vastuulla on kutsua commit-metodia
+        muutosten tallentamiseksi"""
+
         query = """INSERT INTO Links (title, link_url, created_at)
                VALUES (:title, :link_url, datetime('now'))"""
         self.session.execute(query, {"title": link["title"],
                                  "link_url": link["link_url"]})
 
-    """Hae linkki tietokannasta Pythonin dictionary-olion id-kentän
-    perusteella ja poista kyseinen rivi.
-    Huom. Metodin kutsujan vastuulla on kutsua commit-metodia muutosten
-    tallentamiseksi"""
     def delete(self, link):
+        """Hae linkki tietokannasta Pythonin dictionary-olion id-kentän
+        perusteella ja poista kyseinen rivi.
+        Huom. Metodin kutsujan vastuulla on kutsua commit-metodia muutosten
+        tallentamiseksi"""
+
         query = "DELETE FROM Links WHERE id = :id"
         self.session.execute(query, {"id": link["id"]})
 
-    """Hae linkki tietokannasta Pythonin dictionary-olion id-kentän
-    perusteella ja päivitä sen title- ja link_url-kentät.
-    Huom. Metodin kutsujan vastuulla on kutsua commit-metodia muutosten
-    tallentamiseksi"""
     def update(self, link):
+        """Hae linkki tietokannasta Pythonin dictionary-olion id-kentän
+        perusteella ja päivitä sen title- ja link_url-kentät.
+        Huom. Metodin kutsujan vastuulla on kutsua commit-metodia muutosten
+        tallentamiseksi"""
+
         query = """UPDATE Links SET title = :title,
                link_url = :link_url WHERE id = :id"""
         self.session.execute(query, {"id": link["id"],
                                  "title": link["title"] ,
                                  "link_url": link["link_url"]})
 
-    """Kommitoi muutokset tietokantaan"""
+
     def commit(self):
+        """Kommitoi muutokset tietokantaan"""
+
         self.session.commit()
 
-    """Peruuta muutokset tietokantaan"""
     def rollback(self):
+        """Peruuta muutokset tietokantaan"""
+
         self.session.rollback()
