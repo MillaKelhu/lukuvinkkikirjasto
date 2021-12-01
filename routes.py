67825cefc9bb19194644507e7e8960e4e9da0c7b
Riku Mattila <riku.p.mattila@gmail.com>
2021-12-01 21:22:26 +0200

    luotiin web sivulle ulkoasu, lisättiin formi, lisättiin routesiin addlink ja postlink

--- routes.py
+++ routes.py
@@ -3,4 +3,14 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    #argumenttina lukuvinkit = [...] jossa jokainen jäsen sisältää vähintään title ja link_url
+    return render_template("home.html")
+
+@app.route("/addlink")
+def add_link():
+    return render_template("createLink.html")
+
+@app.route("/postlink", methods=["post"])
+def post_link():
+    #formissa title ja link_url
+    pass
--- src/tests/createlink.robot
+++ src/tests/createlink.robot
@@ -2,5 +2,8 @@
 Resource  resource.robot
 Suite Setup  Open And Configure Browser
 Suite Teardown  Close Browser
-
 *** Test Cases ***
+
+Open Link Form Page
+    Go To Add Link Page
+    Add Link Page Should Be Open
