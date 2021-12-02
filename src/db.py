f8d5edffeb6a8e09c3bb0e8fc57c139bd8eef196
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2021-12-02 22:43:52 +0200

    Heroku

--- src/db.py
+++ src/db.py
@@ -5,9 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 def configure_db(app):
     uri = getenv("DATABASE_URL")
     if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)'''
+        uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db = SQLAlchemy(app)
-    
+
     return db
