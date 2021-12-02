290bbf70d02e817442e8a0f4ed34e083ffd1f00a
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2021-12-02 22:38:21 +0200

    Heroku

--- src/db.py
+++ src/db.py
@@ -6,7 +6,7 @@ def configure_db(app):
     '''uri = getenv("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)'''
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://"
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db = SQLAlchemy(app)
     
