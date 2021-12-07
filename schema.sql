97b894d54ead436208c85d2840474bdd67f178ba
Leevi Kokkinen <leevi.kokkinen@gmail.com>
2021-12-07 11:45:26 +0200

    clean repo

--- schema.sql
+++ schema.sql
@@ -10,3 +10,4 @@ CREATE TABLE Users (
     username TEXT UNIQUE,
     password TEXT
 );
+
--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
-from db import configure_db
-from link_repository import LinkRepository
+from src.db import configure_db
+from src.link_repository import LinkRepository
 
 
 app = Flask(__name__)
