3f542b10c3388a9cd0dd715c4e044a300dd89c41
Riku Mattila <riku.p.mattila@gmail.com>
2021-12-09 14:50:40 +0200

    final change for rekisteröityminen

--- src/db.py
+++ src/db.py
@@ -6,10 +6,10 @@ load_dotenv(find_dotenv())
 
 
 def configure_db(app):
-    # uri = os.environ.get("DATABASE_URL")
-    # if uri.startswith("postgres://"):
-    #     uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///lukuvinkkikirjasto"
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db = SQLAlchemy(app)
     return db
