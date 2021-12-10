70937efeb63591b86796479282c9bdf060005437
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2021-12-10 11:34:24 +0200

    migrate db.py to sqlite

--- src/db.py
+++ src/db.py
@@ -7,8 +7,6 @@ load_dotenv(find_dotenv())
 
 def configure_db(app):
     uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db = SQLAlchemy(app)
